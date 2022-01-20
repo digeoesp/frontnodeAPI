76782b17a55488fff03f9aa2aabac34e07ce9fd5
digeoesp <diegocrypto@outlook.com>
2022-01-19 20:05:02 -0800

    hidden files

--- src/App.js
+++ src/App.js
@@ -4,21 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ol>
+        <li>list one</li>
+        <li>list two</li>
+        <li>list thre</li>
+        <li>list four</li>
+      </ol>
+    </div >
   );
 }
 
