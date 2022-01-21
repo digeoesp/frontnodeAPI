0c664a1126251c86be9c7f7d55d227903745679a
digeoesp <diegocrypto@outlook.com>
2022-01-20 21:53:40 -0800

    logic for backend

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Company from './components/Company';
+
+const server = process.env.REACT_APP_SERVER_URL;
+console.log(server)
 
 function App() {
+  const [company, setCompany] = useState({});
+
+  useEffect(() => {
+    const fetchCompany = async () => {
+      const response = await axios.get(server);
+      console.log(response);
+      const data = response.data;
+      console.log(data);
+      //name,author and greeting
+      // destructuring
+      console.log(data);
+      const { name, author, greeting } = data;
+      setCompany({
+        name,
+        author,
+        greeting
+      });
+
+    }
+    fetchCompany();
+  }, [])
+  console.log(company)
   return (
-    <div className="App">
-      <ol>
-        <li>list one</li>
-        <li>list two</li>
-        <li>list thre</li>
-        <li>list four</li>
-        <li>list four</li>
-        <li>list four</li>
-      </ol>
-    </div >
+
+    <Routes>
+      <Route path="/" element={<Company company={company} />} />
+    </Routes>
+
+
+
+
   );
 }
 
