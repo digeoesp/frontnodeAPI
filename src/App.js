0e2d39a65ec82ea02387f3207116d1fb0d1f08d4
digeoesp <diegocrypto@outlook.com>
2022-01-27 00:12:13 -0800

    added new route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import FetchComp from './components/fetchComp/FetchComp';
 import PostFetching from './components/postFile/PostFetching';
 import CommFetching from './components/comment/CommFetching';
+import CreatePost from './components/createPost/CreatePost';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<FetchComp />} />
-        <Route exact path="/posts" element={<PostFetching />} />
-        <Route exact path="/comment" element={<CommFetching />} />
+        <Route path="/posts" element={<PostFetching />} />
+        <Route path="/comment" element={<CommFetching />} />
+        <Route path="posts/create" element={<CreatePost />} />
       </Routes>
     </div>
   );
